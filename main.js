0498d9dfa0f4e4b143e30d3231c1b2bca23d01f1
sagar hussain <160107305+simplegithubr@users.noreply.github.com>
2024-04-02 17:11:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+let todoList = [];
+let loop = true;
+console.log("\n \t wellcome to codewithkinza - Todo-List Application\n");
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            name: "TODO",
+            type: "input",
+            message: "what do you want to add in your todo?"
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want add more todo?",
+            default: false
+        }
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if (TODO) {
+        todoList.push(TODO);
+    }
+    else {
+        console.log("kindly add vaild input");
+    }
+}
+if (todoList.length > 0) {
+    console.log("your todo list : \n");
+    todoList.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todalist found");
+}
